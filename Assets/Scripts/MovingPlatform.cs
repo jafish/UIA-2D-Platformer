7b3565824b1c247e85d34f8af8a94ee1a899e76b
Josh Fishburn <josh@joshfishburn.com>
2018-10-12 12:50:42 -0400

    Added Gizmo showing movement of platform in Scene
    Up through page 142 in Unity in Action Second Edition

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MovingPlatform : MonoBehaviour {
     public Vector3 finishPos = Vector3.zero;
@@ -25,5 +26,10 @@ public class MovingPlatform : MonoBehaviour {
         if ((_direction == 1 && _trackPercent > .9f) || (_direction == -1 && _trackPercent < .1f)) {
             _direction *= -1;
         }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, finishPos);
     }
 }
